5c13aa910b8f392aae4c667986619b1c02953888
Saurabhrana2290 <saurabhrana532@gmail.com>
2021-09-15 21:17:44 +0530

    Updated main.js

--- main.js
+++ main.js
@@ -2,35 +2,32 @@ var billAmount = document.querySelector("#bill-amount");
 var cashGiven = document.querySelector('#cash-given');
 var checkButton = document.querySelector('#check-btn');
 var message = document.querySelector("#error-message");
+var non = document.querySelector(".n");
 var notes=[2000,500,100,20,10,5,1];
 function amountHandler(){
-    message.style.display = "none";
+    
     if(billAmount.value===cashGiven.value){
-        msg = "No amount to return";
-        setMessage(msg);
+        message.innerText = "No amount to return";   
     }
     else if(billAmount.value > 0){
         if(cashGiven.value>billAmount.value){
             var amountDifference = cashGiven.value-billAmount.value;
             calculateAmountReturn(amountDifference);
         }else{
-            msg="Wash the plates!";
+            message.innerText="Wash the plates!";   
         }
     }
     else{
-        msg = "Bill amount should be greater than zero!"
-        setMessage(msg);
+        message.innerText = "Bill amount should be greater than zero!";
+        
     }
 }
-function setMessage(meg){
-    message.style.display = "none";
-    message.innerHTML=msg;
-}
+
 function calculateAmountReturn(amountDifference){
     for(var i=0;i<notes.length;i++){
         var numberOfNotes = Math.trunc(amountDifference/notes[i]);
         amountDifference %= notes[i];
-        console.log(numberOfNotes);
+        non[i].innerText=numberOfNotes;
     }
 }
 
