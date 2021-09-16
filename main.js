76e0e082bc1fe55e043db8976bd14cf21cf8d188
Saurabhrana2290 <saurabhrana532@gmail.com>
2021-09-16 12:49:38 +0530

    Fix logic and table data

--- main.js
+++ main.js
@@ -1,34 +1,40 @@
-var billAmount = document.querySelector("#bill-amount");
-var cashGiven = document.querySelector('#cash-given');
-var checkButton = document.querySelector('#check-btn');
-var message = document.querySelector("#error-message");
-var non = document.querySelector(".n");
-var notes=[2000,500,100,20,10,5,1];
-function amountHandler(){
+
+const checkButton = document.querySelector('#check-btn');
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const message = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+
+const availableNotes = [2000,500,100,20,10,5,1];
+
+checkButton.addEventListener("click", function validateBillAndCashAmount(){
+   hideMessage();
+    if(billAmount.value > 0){
+      if( cashGiven.value >= billAmount.value){
+        var amountToBeReturned = cashGiven.value - billAmount.value;
+        calculateChange(amountToBeReturned);
+      } 
+      else{
+        showMessage("Do you wanna wash the plates?");
+      }
+    }else{
+        showMessage("Bill Amount should be greater than 0");
+    }
+});
+
+function calculateChange(amountToBeReturned){
     
-    if(billAmount.value===cashGiven.value){
-        message.innerText = "No amount to return";   
-    }
-    else if(billAmount.value > 0){
-        if(cashGiven.value>billAmount.value){
-            var amountDifference = cashGiven.value-billAmount.value;
-            calculateAmountReturn(amountDifference);
-        }else{
-            message.innerText="Wash the plates!";   
-        }
-    }
-    else{
-        message.innerText = "Bill amount should be greater than zero!";
-        
+    for(let i=0; i<availableNotes.length; i++){
+        var numberOfNotes = Math.trunc(amountToBeReturned/availableNotes[i]);
+        amountToBeReturned = amountToBeReturned % availableNotes[i];
+        noOfNotes[i].innerText = numberOfNotes;
     }
 }
 
-function calculateAmountReturn(amountDifference){
-    for(var i=0;i<notes.length;i++){
-        var numberOfNotes = Math.trunc(amountDifference/notes[i]);
-        amountDifference %= notes[i];
-        non[i].innerText=numberOfNotes;
-    }
+function hideMessage(){
+    message.style.display = "none";
 }
-
-checkButton.addEventListener("click",amountHandler);
+function showMessage(msg){
+    message.style.display = "block";
+    message.innerText=msg;
+}
