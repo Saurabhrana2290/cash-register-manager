6a351ffc27cb645c5a1ec6393ec8dd94be085090
Saurabhrana2290 <saurabhrana532@gmail.com>
2021-09-16 20:43:06 +0530

    Updated style.css

--- main.js
+++ main.js
@@ -4,37 +4,33 @@ const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const message = document.querySelector("#error-message");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
-
-const availableNotes = [2000,500,100,20,10,5,1];
-
-checkButton.addEventListener("click", function validateBillAndCashAmount(){
-   hideMessage();
-    if(billAmount.value > 0){
-      if( cashGiven.value >= billAmount.value){
-        var amountToBeReturned = cashGiven.value - billAmount.value;
-        calculateChange(amountToBeReturned);
-      } 
-      else{
-        showMessage("Do you wanna wash the plates?");
-      }
-    }else{
-        showMessage("Bill Amount should be greater than 0");
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+checkButton.addEventListener("click", function validateBillAndCashAmount() {
+  hideMessage();
+  if (billAmount.value > 0) {
+    if (cashGiven.value >= billAmount.value) {
+      var amountToBeReturned = cashGiven.value - billAmount.value;
+      calculateChange(amountToBeReturned);
     }
+    else {
+      showMessage("Do you wanna wash the plates?");
+    }
+  } else {
+    showMessage("Bill Amount should be greater than 0");
+  }
 });
+function calculateChange(amountToBeReturned) {
 
-function calculateChange(amountToBeReturned){
-    
-    for(let i=0; i<availableNotes.length; i++){
-        var numberOfNotes = Math.trunc(amountToBeReturned/availableNotes[i]);
-        amountToBeReturned = amountToBeReturned % availableNotes[i];
-        noOfNotes[i].innerText = numberOfNotes;
-    }
+  for (let i = 0; i < availableNotes.length; i++) {
+    var numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+    amountToBeReturned = amountToBeReturned % availableNotes[i];
+    noOfNotes[i].innerText = numberOfNotes;
+  }
 }
-
-function hideMessage(){
-    message.style.display = "none";
+function hideMessage() {
+  message.style.display = "none";
 }
-function showMessage(msg){
-    message.style.display = "block";
-    message.innerText=msg;
+function showMessage(msg) {
+  message.style.display = "block";
+  message.innerText = msg;
 }
