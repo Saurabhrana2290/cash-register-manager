8d145a0f3376c603efc1f9ea120cb66ef32e8554
Saurabhrana2290 <saurabhrana532@gmail.com>
2021-09-17 16:44:54 +0530

    Updated main.js

--- main.js
+++ main.js
@@ -6,24 +6,29 @@ const noOfNotes = document.querySelectorAll(".no-of-notes");
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
   hideMessage();
-  if (billAmount.value > 0) {
-    if (cashGiven.value >= billAmount.value) {
-      var amountToBeReturned = cashGiven.value - billAmount.value;
+  if(billAmount.value > 0) {
+
+    if( billAmount.value < cashGiven.value) {
+      const amountToBeReturned = cashGiven.value - billAmount.value;
       calculateChange(amountToBeReturned);
     }
-    else {
-      showMessage("Do you wanna wash the plates?");
+    else if(billAmount.value===cashGiven.value){
+      showMessage("No amount to be returned!");
     }
-  } else {
-    showMessage("Bill Amount should be greater than 0");
+    else if(cashGiven.value < billAmount.value){
+      showMessage("Cash given less then the bill amount is not acceptable!");
+    }
+
+  } 
+  else{
+    showMessage("Bill Amount should be greater than zero!");
   }
 });
 function calculateChange(amountToBeReturned) {
-
   for (let i = 0; i < availableNotes.length; i++) {
-    var numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
-    amountToBeReturned = amountToBeReturned % availableNotes[i];
+    const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
     noOfNotes[i].innerText = numberOfNotes;
+    amountToBeReturned = amountToBeReturned % availableNotes[i];
   }
 }
 function hideMessage() {
